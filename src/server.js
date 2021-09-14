42d22bcdda19b62e486dd84da951773b6363b8ee
vijay kumar vemana <vijaykumarvemana18@gmail.com>
2021-09-15 00:13:48 +0200

    updated

--- src/server.js
+++ src/server.js
@@ -1,9 +1,13 @@
 import express from "express"
 import authorsRouter from "./services/authors/index.js"
+import listEndpoints from "express-list-endpoints"
 const server = express()
 const port = 3001
 
 server.use("/authors", authorsRouter)
+server.use(express.json())
+console.log(listEndpoints(server))
+console.table(listEndpoints(server))
 
 server.listen(port, () => {
     console.log(`hello portnumber:${port}`)
